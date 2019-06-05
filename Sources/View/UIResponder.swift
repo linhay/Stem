7ea420209789fa410a0819f51df0e5e220e50401
linhey <158179948@qq.com>
2019-06-05 13:26:31 +0800

    0.0.18

---
+++ Sources/View/UIResponder.swift
@@ -0,0 +1,40 @@
+//
+//  Stem
+//
+//  Copyright (c) 2017 linhay - https://github.com/linhay
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+
+import UIKit
+
+public protocol UIResponderRouterEvent { }
+
+public extension UIResponder {
+
+    /// 基于ReponderChain的数据传递
+    /// from: https://casatwy.com/responder_chain_communication.html
+    /// - Parameters:
+    ///   - event: 事件协议
+    ///   - object: 传递对象
+    ///   - userInfo: 传递数据
+    func router(event: UIResponderRouterEvent, object: Any? = nil, userInfo: [String: Any] = [:]) {
+        self.next?.router(event: event, object: object, userInfo: userInfo)
+    }
+
+}
