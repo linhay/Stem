de461410cfa65ef4bcb8b008656e356251a0b375
linhey <158179948@qq.com>
2020-03-25 01:18:30 +0800

    chore: 调整歧义

--- Sources/Foundation/String/String+format.swift
+++ Sources/Foundation/String/String+format.swift
@@ -24,7 +24,6 @@ import Foundation
 // MARK: - format
 
 public extension StemValue where Base == String {
-
     /// format: NSNumber
     var number: NSNumber? { return NumberFormatter().number(from: base) }
     /// format: Int
