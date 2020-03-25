080d76736d4728d84e2002c227d7a6c74355d7d4
linhey <158179948@qq.com>
2020-03-25 19:07:47 +0800

    chore: 添加SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Stem",
+    products: [
+        .library(
+            name: "StemCore",
+            targets: ["Core"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(name: "Core",
+                dependencies: [],
+                path: "Sources/Core"),
+    ]
+)
