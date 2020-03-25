994df41b170d89e86ff7893ef9888ae780e5147e
linhey <158179948@qq.com>
2020-03-26 00:10:19 +0800

    1

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Stem",
+    name: "StemCore",
     products: [
         .library(name: "StemCore",
                  targets: ["Core"]),
