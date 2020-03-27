cc6391a034dfc5f86ac45e81218b25513a24b63d
linhey <158179948@qq.com>
2020-03-27 14:34:07 +0800

    111

--- Sources/Fundamentals/Collections/IndexPath.swift
+++ Sources/Fundamentals/Collections/IndexPath.swift
@@ -25,6 +25,6 @@ extension IndexPath: StemValueCompatible {}
 
 extension StemValue where Base == IndexPath {
 
-    static let zero = IndexPath(item: 0, section: 0)
+    static let zero = IndexPath()
     
 }
