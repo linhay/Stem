e1b1824c9e7efa4cd4e12398099f0426020ed648
linhey <158179948@qq.com>
2020-08-03 15:50:01 +0800

    chore: 结构调整

--- Package/Sources/Package/Package.swift
+++
@@ -1,3 +0,0 @@
-struct Package {
-    var text = "Hello, World!"
-}
--- Package/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import PackageTests
-
-var tests = [XCTestCaseEntry]()
-tests += PackageTests.allTests()
-XCTMain(tests)
