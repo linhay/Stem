a53ca1923d7c246a8c2a55e1a60ac02243c61980
linhey <158179948@qq.com>
2020-08-04 16:40:54 +0800

    fix: 位数错误

--- Sources/CrossPlatform/Color/StemColor.swift
+++ Sources/CrossPlatform/Color/StemColor.swift
@@ -290,11 +290,11 @@ public extension StemColor {
             if count > 8 { break }
         }
         
-        if count == 6 {
+        if count <= 6 {
             self.init(rgb: RGBSpace(red:   Double((value & 0xFF0000) >> 16) / 255,
                                     green: Double((value & 0x00FF00) >> 8) / 255,
                                     blue:  Double(value & 0x0000FF) / 255))
-        } else if count == 8 {
+        } else if count <= 8 {
             self.init(rgb: RGBSpace(red:   Double((value & 0x00FF0000) >> 16) / 255,
                                     green: Double((value & 0x0000FF00) >> 8) / 255,
                                     blue:  Double(value & 0x000000FF) / 255),
