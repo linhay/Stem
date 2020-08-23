4f68adc6f6338e7d2271746ea5420a715b84b7c0
linhey <158179948@qq.com>
2020-08-23 14:22:46 +0800

    chore: 新增 hsb / hsl

--- Sources/CrossPlatform/Color/StemColorSpace+RGB.swift
+++ Sources/CrossPlatform/Color/StemColorSpace+RGB.swift
@@ -9,8 +9,26 @@ import Foundation
 
 public extension StemColor.RGBSpace {
 
-    func red(with value: Double)   -> StemColor.RGBSpace { .init(red: value, green: green, blue: blue)  }
-    func green(with value: Double) -> StemColor.RGBSpace { .init(red: red,   green: value, blue: blue)  }
-    func blue(with value: Double)  -> StemColor.RGBSpace { .init(red: red,   green: green, blue: value) }
+    func red(with value: Double)   -> Self { .init(red: value, green: green, blue: blue)  }
+    func green(with value: Double) -> Self { .init(red: red,   green: value, blue: blue)  }
+    func blue(with value: Double)  -> Self { .init(red: red,   green: green, blue: value) }
 
 }
+
+public extension StemColor.HSBSpace {
+
+    func hue(with value: Double)        -> Self { .init(hue: value, saturation: saturation, brightness: brightness)  }
+    func saturation(with value: Double) -> Self { .init(hue: hue, saturation: value, brightness: brightness)  }
+    func brightness(with value: Double) -> Self { .init(hue: hue, saturation: saturation, brightness: value)  }
+
+}
+
+public extension StemColor.HSLSpace {
+
+    func hue(with value: Double)        -> Self { .init(hue: hue, saturation: saturation, lightness: lightness) }
+    func saturation(with value: Double) -> Self { .init(hue: hue, saturation: value, lightness: lightness)   }
+    func lightness(with value: Double)  -> Self { .init(hue: hue, saturation: saturation, lightness: value)  }
+
+}
+
+
