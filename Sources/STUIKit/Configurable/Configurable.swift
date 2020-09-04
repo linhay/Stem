4ce326bc312030134e97773d5a26d161d07d624a
linhey <158179948@qq.com>
2020-09-04 16:07:37 +0800

    chore: 添加协议

--- Sources/STUIKit/Configurable/Configurable.swift
+++ Sources/STUIKit/Configurable/Configurable.swift
@@ -1,82 +1,91 @@
-/// MIT License
-///
-/// Copyright (c) 2020 linhey
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
+// MIT License
+//
+// Copyright (c) 2020 linhey
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
 
 public protocol ConfigurableView: UIView {
     associatedtype Model
     func config(_ model: Model)
-    static func preferredSize(model: Model?) -> CGSize
-    func preferredSize(model: Model?) -> CGSize
+    static func preferredSize(limit size: CGSize, model: Model?) -> CGSize
 }
 
 public extension ConfigurableView {
 
+    static func preferredSize(model: Model?) -> CGSize {
+        Self.preferredSize(limit: .zero, model: model)
+    }
+
+    static func preferredHeight(width: CGFloat, model: Model?) -> CGFloat {
+        Self.preferredSize(limit: .init(width: width, height: 0), model: model).height
+    }
+
+    static func preferredWidth(height: CGFloat, model: Model?) -> CGFloat {
+        Self.preferredSize(limit: .init(width: 0, height: height), model: model).width
+    }
+
     func preferredSize(model: Model?) -> CGSize {
-        Self.preferredSize(model: model)
+        Self.preferredSize(limit: .zero, model: model)
+    }
+
+    func preferredHeight(width: CGFloat, model: Model?) -> CGFloat {
+        Self.preferredSize(limit: .init(width: width, height: 0), model: model).height
+    }
+
+    func preferredWidth(height: CGFloat, model: Model?) -> CGFloat {
+        Self.preferredSize(limit: .init(width: 0, height: height), model: model).width
     }
 
 }
 
 public extension ConfigurableView where Model == Void {
+
     func config(_ model: Model) { }
 
     static func preferredSize() -> CGSize {
-        preferredSize(model: nil)
+        Self.preferredSize(limit: .zero, model: nil)
+    }
+
+    static func preferredHeight(width: CGFloat) -> CGFloat {
+        Self.preferredSize(limit: .init(width: width, height: 0), model: nil).height
+    }
+
+    static func preferredWidth(height: CGFloat) -> CGFloat {
+        Self.preferredSize(limit: .init(width: 0, height: height), model: nil).width
     }
 
     func preferredSize() -> CGSize {
-        Self.preferredSize(model: nil)
-    }
-}
-
-public protocol ConfigurableCollectionCell: UIView {
-    associatedtype Model
-    func config(_ model: Model)
-    static func preferredSize(collectionView: UICollectionView, model: Model?) -> CGSize
-}
-
-public extension ConfigurableCollectionCell where Model == Void {
-
-    func config(_ model: Model) { }
-
-    static func preferredSize(collectionView: UICollectionView) -> CGSize {
-        preferredSize(collectionView: collectionView, model: nil)
+        Self.preferredSize(limit: .zero, model: nil)
     }
 
-}
+    static func preferredSize(limit size: CGSize) -> CGSize {
+        Self.preferredSize(limit: size, model: nil)
+    }
 
-public protocol ConfigurableTableCell: UIView {
-    associatedtype Model
-    func config(_ model: Model)
-    static func preferredSize(tableView: UITableView, model: Model?) -> CGSize
-}
+    func preferredHeight(width: CGFloat) -> CGFloat {
+        Self.preferredSize(limit: .init(width: width, height: 0), model: nil).height
+    }
 
-public extension ConfigurableTableCell where Model == Void {
-
-    func config(_ model: Model) { }
-
-    static func preferredSize(tableView: UITableView) -> CGSize {
-        preferredSize(tableView: tableView, model: nil)
+    func preferredWidth(height: CGFloat) -> CGFloat {
+        Self.preferredSize(limit: .init(width: 0, height: height), model: nil).width
     }
 
 }
--- Sources/STUIKit/SectionView/Sections/SingleTypeSection.swift
+++ Sources/STUIKit/SectionView/Sections/SingleTypeSection.swift
@@ -1,29 +1,28 @@
-/// MIT License
-///
-/// Copyright (c) 2020 linhey
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
+// MIT License
+//
+// Copyright (c) 2020 linhey
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
-
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
 
-open class SingleTypeSection<Cell: UICollectionViewCell>: SectionCollectionProtocol where Cell: ConfigurableCollectionCell & STViewProtocol {
+open class SingleTypeSection<Cell: UICollectionViewCell>: SectionCollectionProtocol where Cell: ConfigurableView & STViewProtocol {
 
     public private(set) var models: [Cell.Model]
 
@@ -56,7 +55,10 @@ open class SingleTypeSection<Cell: UICollectionViewCell>: SectionCollectionProto
     }
 
     open func itemSize(at row: Int) -> CGSize {
-        return Cell.preferredSize(collectionView: sectionView, model: models.value(at: row))
+        let width = sectionView.bounds.width - sectionInset.left - sectionInset.right
+        return Cell.preferredSize(limit: .init(width: width,
+                                               height: sectionView.bounds.height),
+                                  model: models.value(at: row))
     }
 
     open func didSelectItem(at row: Int) {
