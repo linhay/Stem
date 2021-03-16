8e0639c2c13c7d2b055fc0df591cb645e0713fe3
linhey <158179948@qq.com>
2021-03-16 16:28:24 +0800

    chore: 新增 luminance, linearize 计算

--- Sources/CrossPlatform/Color/StemColor/Spaces/StemColorSpace+RGB.swift
+++ Sources/CrossPlatform/Color/StemColor/Spaces/StemColorSpace+RGB.swift
@@ -23,7 +23,7 @@
 import Foundation
 
 public extension StemColor {
-
+    
     struct RGBSpace: StemColorSpace {
         public private(set) var ranges: [ClosedRange<Double>] = [0...1, 0...1, 0...1]
         // value: 0 - 1.0
@@ -32,7 +32,7 @@ public extension StemColor {
         public let green: Double
         // value: 0 - 1.0
         public let blue: Double
-
+        
         public init(red: Double, green: Double, blue: Double) {
             func map(_ value: Double) -> Double {
                 var value = value
@@ -45,42 +45,67 @@ public extension StemColor {
             self.green = map(green)
             self.blue  = map(blue)
         }
-
     }
-
+    
 }
 
 public extension StemColor.RGBSpace {
+    
+    // See <https://www.w3.org/TR/WCAG20/#relativeluminancedef>
+    func linearize(_ value: Double) -> Double {
+        if (value <= 0.03928) {
+            return value / 12.92
+        }
+        return pow((value + 0.055) / 1.055, 2.4)
+    }
+    
+    /// Returns a brightness value between 0 for darkest and 1 for lightest.
+    ///
+    /// Represents the relative luminance of the color. This value is computationally
+    /// expensive to calculate.
+    ///
+    /// See <https://en.wikipedia.org/wiki/Relative_luminance>.
+    var luminance: Double {
+        // See <https://www.w3.org/TR/WCAG20/#relativeluminancedef>
+        let R = linearize(red)
+        let G = linearize(green)
+        let B = linearize(blue)
+        return 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    }
+    
+}
 
+public extension StemColor.RGBSpace {
+    
     func red(with value: Double)   -> Self { .init(red: value, green: green, blue: blue)  }
     func green(with value: Double) -> Self { .init(red: red,   green: value, blue: blue)  }
     func blue(with value: Double)  -> Self { .init(red: red,   green: green, blue: value) }
-
+    
 }
 
 extension StemColor.RGBSpace: StemColorSpacePack {
-
+    
     public var unpack: (red: Double, green: Double, blue: Double) { (red, green, blue) }
     public var list: [Double] { [red, green, blue] }
     public var intUnpack: (red: Int, green: Int, blue: Int) {
         func map(_ v: Double) -> Int { Int(round(v * 255)) }
         return (map(red), map(green), map(blue))
     }
-
+    
     public init(_ list: [Double]) {
         self.init(red: list[0], green: list[1], blue: list[2])
     }
-
+    
     public init() {
         self.init([0.0, 0.0, 0.0])
     }
-
+    
 }
 
 extension StemColor.RGBSpace: StemColorSpaceRandom {
-
+    
     public static var random: Self { .init([Double.random(in: 0...1),
                                             Double.random(in: 0...1),
                                             Double.random(in: 0...1)]) }
-
+    
 }
