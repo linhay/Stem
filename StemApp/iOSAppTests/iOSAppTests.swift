0f0c75347c966b1d56da48f4d8a60bfc6735f171
linhey <158179948@qq.com>
2021-03-26 14:51:42 +0800

    mmap

--- StemApp/iOSAppTests/iOSAppTests.swift
+++ StemApp/iOSAppTests/iOSAppTests.swift
@@ -10,15 +10,7 @@ import Stem
 
 class iOSAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func testMMAP() throws {
         let filePath = try FilePath(path: "./mmap5.json", inSanbox: .cache, type: .file)
         
         try? filePath.delete()
@@ -27,16 +19,9 @@ class iOSAppTests: XCTestCase {
         let system = filePath.system
         print(filePath.path)
         let result = try system.mmap(size: 1)
-        try result.write(data: "329392932".data(using: .utf8)!)
-        try result.write(data: "sfsdfsdfs".data(using: .utf8)!, offset: 8)
+        try result.write(data: "1234567".data(using: .utf8)!)
+        try result.append(data: "abcdefg".data(using: .utf8)!)
         print(String(data: result.read(), encoding: .utf8)!)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
