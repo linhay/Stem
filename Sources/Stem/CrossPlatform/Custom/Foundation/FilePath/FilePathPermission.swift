4bf87e1828d04dab8880a456794399002c8a11ff
linhey <is.linhey@outlook.com>
2022-03-17 16:19:58 +0800

    添加文件权限枚举

---
+++ Sources/Stem/CrossPlatform/Custom/Foundation/FilePath/FilePathPermission.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by linhey on 2022/3/17.
+//
+
+import Foundation
+
+public struct FilePathPermission: OptionSet {
+    
+    static let exists     = FilePathPermission(rawValue: 1 << 0)
+    static let readable   = FilePathPermission(rawValue: 1 << 1)
+    static let writable   = FilePathPermission(rawValue: 1 << 2)
+    static let executable = FilePathPermission(rawValue: 1 << 3)
+    static let deletable  = FilePathPermission(rawValue: 1 << 4)
+    
+    public let rawValue: Int
+    
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    public init(path: String) {
+        let manager = FileManager.default
+        var list = [FilePathPermission]()
+        
+        guard manager.isExecutableFile(atPath: path) else {
+            self.init(list)
+            return
+        }
+        
+        list.append(.exists)
+        
+        if manager.isReadableFile(atPath: path) {
+            list.append(.readable)
+        }
+        
+        if manager.isWritableFile(atPath: path) {
+            list.append(.writable)
+        }
+        
+        if manager.isDeletableFile(atPath: path) {
+            list.append(.deletable)
+        }
+        
+        if manager.isExecutableFile(atPath: path) {
+            list.append(.executable)
+        }
+        
+        self.init(list)
+    }
+    
+}
