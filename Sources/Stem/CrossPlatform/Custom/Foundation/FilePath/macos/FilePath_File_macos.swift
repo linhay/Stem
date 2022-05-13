221cb17937001f29f085e0e92f0aeebb19b700db
linhey <is.linhey@outlook.com>
2022-05-13 17:33:07 +0800

    update

---
+++ Sources/Stem/CrossPlatform/Custom/Foundation/FilePath/macos/FilePath_File_macos.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by linhey on 2022/5/13.
+//
+
+#if canImport(AppKit)
+import Foundation
+import AppKit
+import SwiftUI
+
+public extension FilePath.File {
+    
+    struct AssociatedApplication {
+        
+        public let url: URL
+        
+        public func icon() -> NSImage {
+            NSWorkspace.shared.icon(forFile: url.path)
+        }
+        
+        public func icon() -> Image {
+            .init(nsImage: icon())
+        }
+    }
+    
+    /// 系统关联 App 列表
+    var associatedApplications: [AssociatedApplication] {
+        let list = (LSCopyApplicationURLsForURL(url as CFURL, .all)?.takeRetainedValue() as? [URL]) ?? []
+        return list.map(AssociatedApplication.init(url:))
+    }
+    
+}
+#endif
---
+++ StemApp/iOSApp/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  iOS
+//
+//  Created by 林翰 on 2021/3/1.
+//  Copyright © 2021 linhey.ax. All rights reserved.
+//
+
+import UIKit
+import Stem
+import Foundation
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
