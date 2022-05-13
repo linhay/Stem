c4a2ae09fc0336e3399019893ad86d806494277c
linhey <is.linhey@outlook.com>
2022-05-13 18:00:19 +0800

    update

--- Sources/Stem/CrossPlatform/Custom/Foundation/FilePath/macos/FilePath_File_macos.swift
+++ Sources/Stem/CrossPlatform/Custom/Foundation/FilePath/macos/FilePath_File_macos.swift
@@ -13,9 +13,19 @@ import SwiftUI
 public extension FilePath.File {
     
     struct AssociatedApplication: Identifiable, Equatable, Hashable {
-        public var id: URL { url }
         
+        public var id: URL { url }
         public let url: URL
+        public var bundle: Bundle { .init(path: url.path)! }
+        public var name: String {
+            if let name = bundle.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
+                return name
+            } else if let name = bundle.object(forInfoDictionaryKey: kCFBundleNameKey as String) as? String {
+                return name
+            } else {
+                return url.pathComponents.last ?? url.path
+            }
+        }
         
         public func icon() -> NSImage {
             NSWorkspace.shared.icon(forFile: url.path)
@@ -24,6 +34,14 @@ public extension FilePath.File {
         public func icon() -> Image {
             .init(nsImage: icon())
         }
+        
+        public func open(_ files: [FilePath.File]) {
+            NSWorkspace.shared.open(files.map(\.url), withApplicationAt: url, configuration: NSWorkspace.OpenConfiguration())
+        }
+    }
+    
+    func open(with app: AssociatedApplication) {
+        app.open([self])
     }
     
     /// 系统关联 App 列表
